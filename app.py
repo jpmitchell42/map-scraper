5a81312e3202a7f855203057773ebded2e6e7e38
James Mitchell <james.p.mitchell5@gmail.com>
2019-08-17 12:09:38 -0700

    pulls down necessary details

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from config import Config
 import boto3
 import requests
-from one import MapDetails
+from page import MapDetails
 from utils.bucket import Bucket
 
 
 def create_app():
-    ddb = boto3.resource('dynamodb', region_name='us-east-1')
+    ddb = boto3.resource("dynamodb", region_name="us-east-1")
     table = ddb.Table(Config.TABLE_NAME)
     b = Bucket()
     m = MapDetails(
-        table=table, page_link='https://www.raremaps.com/gallery/detail/54058/angliae-scotiae-et-hiberniae-sive-britannicae-insularum-de-ortelius', bucket=b, run_on_init=False)
-    m.write_details_to_db()
+        table=table,
+        page_link="https://www.raremaps.com/gallery/detail/54058/angliae-scotiae-et-hiberniae-sive-britannicae-insularum-de-ortelius",
+        bucket=b,
+        run_on_init=False,
+    )
     m.read_info_card()
+    m.write_details_to_db()
 
 
 if __name__ == "__main__":
